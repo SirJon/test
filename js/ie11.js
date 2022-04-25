14b965c363c0ed5707bea56f0253e70352f2bea5
Evgeny <ew.silin@gmail.com>
2022-04-25 16:54:03 +0300

    update for ie11

---
+++ js/ie11.js
@@ -0,0 +1,34 @@
+(function(){
+  "use strict";
+  
+  const writeTime = function(hourItem, minutesItem, secondsItem){  
+    const Data = new Date();
+    let hour = Data.getHours();
+    let minutes = Data.getMinutes();
+    let seconds = Data.getSeconds();    
+
+    hourItem.textContent = hour;
+    minutesItem.textContent = minutes;
+    secondsItem.textContent = seconds;
+  }
+
+  const creatTime = function(){
+    return (
+      "<div class='time'><ul class='time__list'><li id='hour' class='time__item'>00</li><li class='time__item time__item--delimiter'>:</li><li id='minutes' class='time__item'>00</li><li class='time__item time__item--delimiter'>:</li><li id='seconds' class='time__item'>00</li></ul></div>"
+    );
+  };
+
+  const render = function(container, position, text){
+    container.insertAdjacentHTML(position, text);
+  };
+  const headerBlock = document.querySelector(".page-main");
+  render(headerBlock, "beforeend", creatTime());  
+
+  const hourItem = document.getElementById("hour");
+  const minutesItem = document.getElementById("minutes");
+  const secondsItem = document.getElementById("seconds");  
+
+  setInterval(function(){
+    writeTime(hourItem, minutesItem, secondsItem)
+  }, 1000);
+})();
